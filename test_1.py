1e34669f5bfca44a3a705502303f4a94577bafd3
Anastasia <123269644+Anastasi-Markina@users.noreply.github.com>
2023-12-14 23:37:24 +0300

    Create test_1.py

---
+++ test_1.py
@@ -0,0 +1,12 @@
+import pytest
+from check_post import get_post
+
+id_check = 91749
+
+
+def test_1(token):
+    output = get_post(token)['data']
+    res = []
+    for item in output:
+        res.append(item['id'])
+        assert id_check in res
